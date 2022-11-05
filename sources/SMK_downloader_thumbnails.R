274dda14140d49e33c4937e18394d9a952174fc1
maxodsbjerg <mape168@gmail.com>
2022-11-05 16:36:10 +0100

    Added notebook and sources file for downloading a visualising pictures

---
+++ sources/SMK_downloader_thumbnails.R
@@ -0,0 +1,25 @@
+SMK_downloader <- function(dataframe){
+  
+  outputdir <- "../data/images/"
+  print(getwd())
+  
+  if (!dir.exists("../data/images/")){
+    dir.create("../data/images/")
+  } else {
+    print("Dir already exists!")
+  }
+  
+  for (i in 1:nrow(dataframe)) {
+    id <- dataframe[i, "id"][[1]]
+    thumb <- dataframe[i, "image_thumbnail"][[1]]
+    
+    img <- load.image(thumb)
+    
+    save.image(img, paste0("../data/images/", id, ".png"))
+  }
+
+  filenames <- tibble(filenames = list.files("../data/images/", full.names = TRUE)) %>% 
+    mutate(id = str_replace(filenames, ".*\\/(\\d+_\\w+)\\.\\w+$", "\\1"))
+  
+    right_join(filenames, dataframe, by = "id")
+}
